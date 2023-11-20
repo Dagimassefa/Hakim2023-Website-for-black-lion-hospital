aa0d699b3abbc458bc66628e181e937ad96bd21d
Dagim Assefa <94830418+Dagimassefa@users.noreply.github.com>
2023-11-20 13:26:08 +0300

    Update configure.php

--- configure.php
+++ configure.php
@@ -1,6 +1,6 @@
 <?php 
 
-   $connection = mysqli_connect('localhost', 'tsionawp_tsionawp', 'w.16H]@s3vXDYGg)7@Qv', 'tsionawp_hakim') or die("not connected" . mysqli_error());
+   $connection = mysqli_connect('localhost', 'root', '', 'dbname') or die("not connected" . mysqli_error());
 mysqli_set_charset($connection, "utf8mb4");
 
 
